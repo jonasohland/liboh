9fdf0efe55e9df6968a886474fa130bcf8e911af
Jonas Ohland <jonas.ohland@gmail.com>
2019-04-03 15:24:23 +0200

    doc

--- include/o/sequence.h
+++ include/o/sequence.h
@@ -3,6 +3,17 @@
 
 namespace o {
 
+    /**
+     * Emplace n elements to the back of a sequence
+     *
+     * @tparam  Sequence    Type of the sequence.
+     * @tparam  Count       Type of the count.
+     * @tparam  Args        Type of the arguments.
+     * @param [in,out]  seq     The sequence.
+     * @param           c       The Number of elements
+     * @param           args    Variable arguments providing the
+     *                          arguments to emplace_back on the sequence.
+     */
     template <typename Sequence, typename Count, typename... Args>
     void emplace_n(Sequence& seq, Count c, Args&&... args) {
         for (auto i = 0; i < c; ++i) {
