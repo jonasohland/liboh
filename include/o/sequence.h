04c795183d7c1719a40a7794db6bd4ad880b2eba
Jonas Ohland <jonas.ohland@gmail.com>
2019-04-03 15:21:36 +0200

    emplace_n arg forwarding

--- include/o/sequence.h
+++ include/o/sequence.h
@@ -4,7 +4,7 @@
 namespace o {
 
     template <typename Sequence, typename Count, typename... Args>
-    void emplace_n(Sequence& seq, Count c, Args... args) {
+    void emplace_n(Sequence& seq, Count c, Args&&... args) {
         for (auto i = 0; i < c; ++i) {
             seq.emplace_back(std::forward<Args>(args)...);
         }
